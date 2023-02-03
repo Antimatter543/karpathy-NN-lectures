2bf3ed82315377defc170f0735ab15fca5827c06
Anti Matter <spiralknightsowner@gmail.com>
2023-02-03 18:16:43 +1000

    Added one scaled attention head to model in v2

--- 7 - nanogpt/v2.py
+++ 7 - nanogpt/v2.py
@@ -5,9 +5,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32 
@@ -58,14 +58,41 @@ def estimate_loss():
     model.train()
     return out
 
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,C)
+        q = self.query(x) # (B,T,C)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T) || Scaled attention
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T) || Makes this a decoder block (cannot talk to future)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,C)
+        out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+        return out
+
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # Number of dimensions we want for each vocab
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # Number of dimensions we want for each vocab. Legit just a lookup table.
         self.position_embedding_table = nn.Embedding(block_size, n_embd) # || We want to embed the positions too
+        self.sa_head = Head(n_embd) # lets us consider previous tokens with different affinities/strengths.
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -75,7 +102,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C). Embeds basically every number 0 - T-1 into the table to make (T,C).
         x = tok_emb + pos_emb # (B,T,C) + (T,C) -> right aligns TC to (1,T,C), then broadcasts across the batch dimension (because well, over the batch the position embeds would be the same).
-        
+        x = self.sa_head(x) # apply one head of self-attention (B,T,C)
         logits = self.lm_head(x) # (B,T, vocab_size)
         
         if targets is None:
@@ -91,8 +118,13 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            
+            # crop idx to last #block_size tokens (like 'scrolling') bc we have positional embeddings now (as our positional emb. table only has embeds for up to block size)
+            idx_crop = idx[:, :block_size]
+
+
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_crop)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
